9ee9d302771bd50d862d19d4724360c7707f4ea3
Cuteghost3 <113481723+Cuteghost3@users.noreply.github.com>
2022-11-01 12:44:27 -0600

    V1_BooGame
    Menus
    Dragg
    Clik Count
    Cliker

---
+++ Boo-Ghost/Assets/Scrips/SoulsTaken.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoulsTaken : MonoBehaviour
+{
+    public int Souls;
+
+    public Text SoulsDisplay;
+
+    public void ClickSouls()
+    {
+        Souls += 1;
+
+        SoulsDisplay.text = "" + Souls; 
+    }
+}
